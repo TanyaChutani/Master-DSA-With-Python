ca418631c94113cfa3c01fd95da8a59435cb4811
Tanya <tanyach1997@gmail.com>
2022-04-14 19:51:18 +0530

    Update valid_parentheses.py

--- string/easy/valid_parentheses.py
+++ string/easy/valid_parentheses.py
@@ -1,3 +1,9 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        
+        if len(s) == 0: return false
+        open_bracket = {")":"(", "}":"{", "]":"["}
+        stack = []
+        for i in s:
+            if i in open_bracket.values():
+                stack.append(i)
+            else:
