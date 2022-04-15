f5268ea197ccface54191d793dc41022ca088a2d
Tanya <tanyach1997@gmail.com>
2022-04-16 01:12:53 +0530

    Update valid_parentheses.py

--- string/easy/valid_parentheses.py
+++ string/easy/valid_parentheses.py
@@ -6,4 +6,12 @@ class Solution:
         for i in s:
             if i in open_bracket.values():
                 stack.append(i)
-            else:
+            elif len(stack) == 0 and i in open_bracket.keys():
+                return False
+            elif open_bracket[i] == stack[len(stack)-1] and len(stack)>0:
+                stack.pop()
+            elif open_bracket[i] != stack[len(stack) - 1] and len(stack)>0:
+                return False
+            
+        if len(stack)==0:
+            return True
