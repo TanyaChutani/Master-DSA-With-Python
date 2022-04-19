5d2b5b2cbd01426495bbd576bfa78338361c71e8
Tanya <tanyach1997@gmail.com>
2022-04-19 13:52:43 +0530

    Update valid_palindrome.py

--- string/easy/valid_palindrome.py
+++ string/easy/valid_palindrome.py
@@ -1,12 +1,14 @@
 import re
+
+
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         sentence = ("".join(re.findall("[a-zA-Z0-9]+", s))).lower()
         length_sentence = len(sentence)
-        if length_sentence == 0: return True
-        if length_sentence == 1: return True
-        for i in range(length_sentence//2):
-            if sentence[i] == sentence[length_sentence-i-1]:
+        if length_sentence == 0 or length_sentence == 1:
+            return True
+        for i in range(length_sentence // 2):
+            if sentence[i] == sentence[length_sentence - i - 1]:
                 pass
             else:
                 return False
