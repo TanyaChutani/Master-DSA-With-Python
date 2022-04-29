51c58b192650d396606dd3c2bfe5f77df1abc0f6
Tanya <tanyach1997@gmail.com>
2022-04-30 03:02:27 +0530

    Update majority_element.py

--- array/easy/majority_element.py
+++ array/easy/majority_element.py
@@ -1,3 +1,5 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        
+        hash_map = {i: nums.count(i) for i in set(nums)}
+        final_output = sorted(hash_map.items(), key=lambda x: x[1], reverse=True)[0][0]
+        return final_output
