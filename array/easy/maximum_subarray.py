5194fcd3ab716867202ad5cb45f7e94ce479921d
Tanya <tanyach1997@gmail.com>
2022-05-11 13:27:10 +0530

    Update maximum_subarray.py

--- array/easy/maximum_subarray.py
+++ array/easy/maximum_subarray.py
@@ -1,3 +1,12 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        
+        sum_subarray = 0
+        max_subarray = nums[0]
+
+        for i in nums:
+            sum_subarray += i
+            if sum_subarray > max_subarray:
+                max_subarray = sum_subarray
+            if sum_subarray < 0:
+                sum_subarray = 0
+        return max_subarray
