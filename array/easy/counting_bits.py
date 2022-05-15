1261729dab87cf2ab8052ae4087ff5905fa40313
Tanya <tanyach1997@gmail.com>
2022-05-15 18:16:09 +0530

    Update counting_bits.py

--- array/easy/counting_bits.py
+++ array/easy/counting_bits.py
@@ -1,3 +1,12 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        
+        final_list = []
+        for i in range(n+1):
+            if i == 0:
+                final_list.append(0)
+            elif i%2 == 0:
+                final_list.append(final_list[i//2])
+            else:
+                final_list.append(final_list[i//2] + 1)
+        return final_list
+            
