e4a74213692e01d3388176293bf6ee1ff907ad30
Tanya <tanyach1997@gmail.com>
2022-05-26 21:05:29 +0530

    Update move_zeroes.py

--- array/easy/move_zeroes.py
+++ array/easy/move_zeroes.py
@@ -1,1 +1,11 @@
-
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        non_zero = 0
+        
+        for i in nums:
+            if i != 0:
+                nums[non_zero] = i
+                non_zero += 1
+        for i in range(non_zero, len(nums)):
+            nums[i] = 0
+            
