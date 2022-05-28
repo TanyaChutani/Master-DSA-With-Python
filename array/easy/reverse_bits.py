f60a5b72a423d12ad52ae2a37c0deea809a06128
Tanya <tanyach1997@gmail.com>
2022-05-28 21:22:39 +0530

    Update reverse_bits.py

--- array/easy/reverse_bits.py
+++ array/easy/reverse_bits.py
@@ -1,1 +1,8 @@
-
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        final_output = 0
+        for _ in range(0,32):
+            final_output <<= 1
+            final_output = final_output | (n & 1)
+            n >>= 1
+        return final_output
