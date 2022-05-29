a0a06af7a4e66f1dab4b1c037aa7d7dd31a883a0
Tanya <tanyach1997@gmail.com>
2022-05-30 00:18:19 +0530

    Update add_digits.py

--- array/easy/add_digits.py
+++ array/easy/add_digits.py
@@ -1,1 +1,9 @@
-
+class Solution:
+    def addDigits(self, num: int) -> int:
+        nums = list(map(int, str(num)))
+        if len(nums)>1:
+            nums = sum(nums)
+            nums = self.addDigits(nums)
+        else:
+            nums = nums[0]
+        return nums
