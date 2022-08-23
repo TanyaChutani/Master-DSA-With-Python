d2815fbc83a83ff7bfdf77817374fe9b946e69f2
Tanya <tanyach1997@gmail.com>
2022-08-23 15:34:28 +0530

    Update happy_number.py

--- array/easy/happy_number.py
+++ array/easy/happy_number.py
@@ -1,1 +1,14 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        visited_n = []
+        while n not in visited_n:
+            visited_n.append(n)
+            sum_number = 0
+            while n:
+                sum_number += (n % 10) ** 2
+                n //= 10
+            n = sum_number
+            if n == 1:
+                return True
+        return False
 
