a4525fed2dd3d5cd5618c60b240db60a7cd393f2
Tanya <tanyach1997@gmail.com>
2022-09-06 00:09:05 +0530

    Update strictly_palindromic_number.py

--- array/medium/strictly_palindromic_number.py
+++ array/medium/strictly_palindromic_number.py
@@ -1,1 +1,13 @@
+class Solution:
+    def isStrictlyPalindromic(self, n: int) -> bool:
+        for i in range(2, n - 1):
+            new_n = i ** n
+            reverse = 0
+            while new_n > 0:
+                digit = new_n % 10
+                reverse = reverse * 10 + digit
+                new_n = new_n // 10
+            if i ** n != reverse:
+                return False
+        return True
 
