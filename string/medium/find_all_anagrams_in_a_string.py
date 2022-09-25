dc0b1009aae8d2419551fb187d6dd182262a102d
Tanya <tanyach1997@gmail.com>
2022-09-25 22:12:00 +0530

    Update find_all_anagrams_in_a_string.py

--- string/medium/find_all_anagrams_in_a_string.py
+++ string/medium/find_all_anagrams_in_a_string.py
@@ -1,1 +1,21 @@
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        
+        phash = (dict.fromkeys(p+s, 0))
+        shash = (dict.fromkeys(p+s, 0))
+        final_result = []
+        
+        for i in p:
+            phash[i] += 1
 
+        for i in s[:len(p)]:
+            shash[i] += 1
+        if phash == shash:
+            final_result.append(0)
+            
+        for idx, j in enumerate(s[1:-(len(p)-1) if len(p)!=1 else len(s)]):
+            shash[s[idx]]-=1
+            shash[s[idx+len(p)]]+=1
+            if phash == shash:
+                final_result.append(idx+1)
+        return final_result
