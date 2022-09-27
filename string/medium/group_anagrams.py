ea0c4e10d5420e0a13617a6e04cb333502e246dc
Tanya <tanyach1997@gmail.com>
2022-09-27 21:47:56 +0530

    Update group_anagrams.py

--- string/medium/group_anagrams.py
+++ string/medium/group_anagrams.py
@@ -1,1 +1,8 @@
+from collections import defaultdict
 
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        final_output = defaultdict(list)
+        for w in strs:
+            final_output["".join(sorted(w))].append(w)
+        return final_output.values()
