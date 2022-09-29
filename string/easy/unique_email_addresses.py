6678c761d8943e04cb352ebaccc49a881d021021
Tanya <tanyach1997@gmail.com>
2022-09-29 15:44:59 +0530

    Update unique_email_addresses.py

--- string/easy/unique_email_addresses.py
+++ string/easy/unique_email_addresses.py
@@ -1,1 +1,11 @@
-
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        final_output = {}
+        for i in emails:
+            domain = i.split("@")[-1]
+            if "+" in i:
+                i =  i.split("+")[0] + "@" + domain
+            if "." in i:
+                i =  i.replace(".", "") + "@" + domain
+            final_output[i] = 1
+        return len(final_output)
