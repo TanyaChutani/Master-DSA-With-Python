8940ac0e51f054f65cf61661e3665a9c31124eea
Tanya <tanyach1997@gmail.com>
2022-10-02 21:07:01 +0530

    Update next_permutation.py

--- array/medium/next_permutation.py
+++ array/medium/next_permutation.py
@@ -1,1 +1,11 @@
-
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        if len(nums) <= 1:
+            return
+        for idx, i in enumerate(nums[:-2]):
+            if nums[idx] <= nums[idx + 1]:
+                idx1 = idx + 1
+                for idx_reverse in reversed(range(len(nums))):
+                    if nums[idx_reverse] >= nums[idx1]:
+                        nums[idx1], nums[idx_reverse] = nums[idx_reverse], nums[idx1]
+                nums[idx1:-1].reverse()
