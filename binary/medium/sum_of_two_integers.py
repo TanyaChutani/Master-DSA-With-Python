8badf04ee8be4b94a88d7fdaaa206f8d80ae2cae
Tanya <tanyach1997@gmail.com>
2022-11-14 21:23:42 +0530

    Update sum_of_two_integers.py

--- binary/medium/sum_of_two_integers.py
+++ binary/medium/sum_of_two_integers.py
@@ -1,1 +1,9 @@
-
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        carry = b
+        while carry!=0:
+            xor = a ^ b
+            carry = (a & b) << 1
+            b = carry
+            a = xor
+        return xor
