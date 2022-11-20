c9d8d919d01e55e28a7087c80bc000b221359f4e
Tanya <tanyach1997@gmail.com>
2022-11-20 17:05:19 +0530

    Update counting_bits.py

--- binary/easy/counting_bits.py
+++ binary/easy/counting_bits.py
@@ -1,1 +1,6 @@
-
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        output = [0]
+        for i in range(1, n+1):
+            output.append(output[i>>1] + i%2)
+        return output
