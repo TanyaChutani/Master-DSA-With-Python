f8e53d0238775c6f659a0a57ae3682d330d531b6
Tanya <tanyach1997@gmail.com>
2023-01-17 23:15:34 +0530

    Update climbing_stairs.py

--- dynamic_programming/easy/climbing_stairs.py
+++ dynamic_programming/easy/climbing_stairs.py
@@ -1,1 +1,6 @@
-
+class Solution(object):
+    def climbStairs(self, n):
+        dp = [0, 1, 2]
+        for i in range(3, n + 1):
+            dp.append(dp[i - 1] + dp[i - 2])
+        return dp[n]
