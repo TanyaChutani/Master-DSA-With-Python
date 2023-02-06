477b77a70ca607f2ddd5bf550829c06dbea366cb
Tanya <tanyach1997@gmail.com>
2023-02-06 17:11:26 +0530

    Update selection_sort.py

--- sorting/selection_sort.py
+++ sorting/selection_sort.py
@@ -1,1 +1,11 @@
+def selectionSort(array, size):
+   
+    for j in range(size):
+        min_idx = j
 
+        for i in range(j + 1, size):
+         
+            if array[i] < array[min_idx]:
+                min_idx = i
+         
+        (array[j], array[min_idx]) = (array[min_idx], array[j])
