8d8060b1bb7dd260a5fb7fca91f2d86b417b10d5
Tanya <tanyach1997@gmail.com>
2023-02-20 15:20:18 +0530

    Update bubble_sort.py

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -1,1 +1,8 @@
-
+def bubblesort(array, size):
+    for i in range(size):
+        for j in range(size-i-1):
+         
+            if array[j] > array[j+1]:         
+               (array[j], array[j+1]) = (array[j+1], array[j])
+      
+    return array
